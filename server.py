21bf25613777656d364a33066c12cda4c3ffe8d6
Jurgen Stegeman <jurgenstegeman@live.nl>
2023-12-28 13:36:36 +0100

    Add contact.html

--- server.py
+++ server.py
@@ -13,5 +13,10 @@ def projects():
     return render_template("projects.html")
 
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
